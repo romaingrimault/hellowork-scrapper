ce1f9bd61b0eee6cc789ac9949156efe6de0af46
Romain <46965933+romaingrimault@users.noreply.github.com>
2023-10-19 17:55:12 +0200

    Fix: modification du xPath du span de ref

--- app/src/main/java/fr/romaingrimault/scrapper/service/ScrapperService.java
+++ app/src/main/java/fr/romaingrimault/scrapper/service/ScrapperService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
@@ -12,11 +13,16 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+import fr.romaingrimault.scrapper.model.Offer;
+
 
 @Service
 public class ScrapperService {
 
     private final WebClient webClient = new WebClient(BrowserVersion.CHROME);;
+
+    @Autowired
+    private OfferService offerService;
 
     public void scrapUrl(String url) throws Exception{
         this.webClient.getOptions().setCssEnabled(false);
@@ -26,15 +32,17 @@ public class ScrapperService {
             HtmlPage page = this.webClient.getPage(url);
             ArrayList<String> offersUrl = this.getOfferUrl(page);
             offersUrl.forEach( offerUrl -> {
-                System.out.println("url: "+offerUrl);
-
                 try {
-
                     String ref = this.getRef(this.webClient.getPage(offerUrl));
-                    System.out.println("Ref: "+ref);
-                    System.out.println("\n--------\n");
-                    // insert to bdd
-
+                    if(!ref.isEmpty() && !offerService.isOffer(ref)){
+                        System.out.println("create offer wth ref "+ref);
+                        Offer offer = new Offer(offerUrl, ref);
+                        offerService.addOffer(offer);    
+                    }
+                    else{
+                        System.out.println("No ref find");
+                    }
+                    
                 } catch (Exception e) {
                   System.out.println(e);
                 }
@@ -65,7 +73,7 @@ public class ScrapperService {
 
     private String getRef(HtmlPage page){
         String ref = "";
-            String xPath = "//div[@class='tw-col-span-full lg:tw-col-span-4 md:tw-mb-12']//section/span[last()]";
+            String xPath = "//div[@class='tw-layout-inner-grid']/div[last()]//section/span[last()]";
             List<DomNode> offersList = (List<DomNode>) page.getByXPath(xPath);
             if(offersList.size() == 1){
                 String htmlText = offersList.get(0).asText();
